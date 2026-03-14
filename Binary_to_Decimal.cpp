2e0c0ee86a19d3a6dad3080ca73740f6fec8b1ab
Arnab Jana <arnab003jn.369@gmail.com>
2026-03-14 20:45:44 +0530

    updated

--- Binary_to_Decimal.cpp
+++ Binary_to_Decimal.cpp
@@ -5,7 +5,7 @@ int main(){
    int number,ans = 0, i = 0;
    cout << "Enter a Binary Number: ";
    cin >> number;
-   while(number > 0)   {
+   while(number)   {
       int rem = number%10;
       number /= 10;
       if(rem == 1)
